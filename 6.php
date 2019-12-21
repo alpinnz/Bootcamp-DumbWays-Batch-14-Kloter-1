742b0e92adcbfc3df9eab8334cc179f61d746421
Alpinnz <alpinnz@gmail.com>
2019-12-21 17:20:14 +0700

    page deploy

---
+++ 6.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/bootstrap-4.3.1/css/bootstrap.min.css">
+    <!-- Fontawalsome CSS -->
+    <link rel="stylesheet" href="assets/fontawesome-5.11.2/css/all.css">
+    <!-- style -->
+    <link rel="stylesheet" href="assets/style.css">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <div class="wrapper m-4">
+        <div class="header">
+            <div class="container-fluid text-center shadow-sm">
+                <h1 class="border-bottom text-dark">Pemilihan Kadidat</h1>
+            </div>
+        </div>
+        <div class="content mt-4">
+            <div class="container">
+                <table class="table table-bordered text-center align-middle border-0 shadow-lg">
+                    <thead>
+                        <tr>
+                            <th scope="col" colspan="2">Caleg</th>
+                            <th scope="col" colspan="2">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th class="text-left" scope="row" colspan="2">
+                                nama
+                            </th>
+                            <th scope="row" colspan="2" rowspan="4">
+                                <button type="button" class="btn btn-danger">TOMBOL TAMBAH</button>
+                            </th>
+                        </tr>
+                        <tr>
+                            <th class="text-left" scope="row" colspan="2">
+                                Perolehan suara:
+                            </th>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="assets/jquery-3.4.1.slim.min.js"></script>
+    <script src="assets/popper.min.js"></script>
+    <script src="assets/bootstrap-4.3.1/js/bootstrap.min.js"></script>
+    <script src="assets/main.js"></script>
+</body>
+
+</html>
