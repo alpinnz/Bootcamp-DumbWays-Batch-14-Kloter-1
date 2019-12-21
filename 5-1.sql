49aba1f5da5a41cf92b8fada6baf605059a5d0bf
Alpinnz <alpinnz@gmail.com>
2019-12-21 16:23:21 +0700

    Update 5-1.sql

--- 5-1.sql
+++ 5-1.sql
@@ -1,3 +1,5 @@
+-- Buatlah suatu database dengan nama count pada database tersebut terdapat 2 tabel dengan spesifikasi sebagai berikut :
+
 CREATE DATABASE count;
 
 CREATE TABLE tb_caleg(id int(25) NOT NULL PRIMARY KEY, name varchar(25) NOT NULL, id_partai int(25) NOT NULL, earned_vote int(25));
@@ -10,6 +12,27 @@ INSERT INTO tb_caleg (id, name, id_partai, earned_vote) VALUES (1, 'Sera', 2, 10
 
 INSERT INTO tb_partai (id, name) VALUES (1, 'Cilacap Santai'), (2, 'Cilacap Sejahtera');
 
+-- Tampilkan isi dari masing masing tabel.
 SELECT * FROM tb_partai;
-
 SELECT * FROM tb_caleg;
+
+-- Tambahkan 1 data partai dengan nama “Cilacap Aman”.
+INSERT INTO tb_partai (id, name) VALUES (3, 'Cilacap Aman');
+
+-- Rubahlah nama partai Cilacap Sejahtera menjadi Cilacap Makmur.
+UPDATE tb_partai SET name = "Cilacap Makmur" WHERE id=2;
+
+-- Tambahkan 2 data pada caleg dengan nama “Kabur” lalu partai yang di usung “Cilacap Aman”. “Astina” partai yang di usung “Cilacap Aman”.
+INSERT INTO tb_caleg (id, name, id_partai, earned_vote) VALUES (3, 'Kabur', 3, 0), (4, 'Astina', 3, 0);
+
+-- Ubah dan tambahkan earn vote pada data “Kabur” sebesar “69” dan pada data “Astina” sebesar “21” 
+UPDATE tb_caleg SET earned_vote = "69" WHERE id=3;
+UPDATE tb_caleg SET earned_vote = "21" WHERE id=4;
+
+-- Ubahlah 1 caleg dengan nama Sera dengan ketentuan berikut, “Is Bos”,”2”,”70”
+UPDATE tb_caleg SET name="Is Bos", id_partai="2", earned_vote = "70" WHERE id=1;
+
+-- Hapus caleg dengan nama “Astina”.
+DELETE tb_caleg WHERE id=4;
+
+
